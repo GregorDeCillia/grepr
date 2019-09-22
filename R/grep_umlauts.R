9bb7865d26ea1df11caaa918b047f4dbff47d3b5
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-09-22 21:06:13 +0200

    grep_umlauts

---
+++ R/grep_umlauts.R
@@ -0,0 +1,23 @@
+specials <- "[\u00c4\u00e4\u00d6\u00f6\u00dc\u00fc\u00df]"
+
+#' Mark all umlauts
+#'
+#' Searches for umlauts (ÖÄÜöäü) with [grepr()] and creates RStudio markers
+#' @inheritParams grepr
+#' @param ... arguments which are passed to [grepr()]
+#' @param ignore_roxygen do not show umlauts in roxygen blocks or Rd files
+#' @export
+grep_umlauts <- function(pattern = specials, ..., ignore_roxygen = FALSE) {
+  matches <- grepr(pattern = pattern, ...)
+  if (ignore_roxygen)
+    matches <- drop_roxygen_matches(matches)
+  matches
+}
+
+drop_roxygen_matches <- function(matches) {
+  dir <- attr(matches, "dir")
+  matches <- matches[substr(matches$message, 1, 2) != "#'", ]
+  matches <- matches[substr(matches$file, 1, 4) != "man/", ]
+  attr(matches, "dir") <- dir
+  matches
+}
